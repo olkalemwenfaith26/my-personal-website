33a233eb77f0902194de59aef5a482deb547bf7e
olkalemwen faith <olkalemwenfaith26@gmail.com>
2025-11-25 14:13:52 +0100

    implemented next round work flow .

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -63,16 +63,22 @@ function hideMainGameSection () {
 }
 
 function setupFirstRound () {
-    document.getElementById(crapsRollDiceAnimationContainer).style.display = "none"
-    document.getElementById(crapsRoundFinishGridContainer).style.display = "none"
-    document.getElementById (crapsRollDiceButton).style.display = "block"
-    document.getElementById (crapsBettingGridContainer).style.display = "block"
     document.getElementById(crapsStatsUsername).innerHTML = crapsUsername
-    canChangeBet = true
     setMoney(startingMoney)
     setRounds(startingRounds)
     betEven()
     setBetAmount(minimumBet)
+    setupNextRound()
+}
+
+function setupNextRound () {
+    document.getElementById(crapsRollDiceAnimationContainer).style.display = "none"
+    document.getElementById(crapsRoundFinishGridContainer).style.display = "none"
+    document.getElementById (crapsRollDiceButton).style.display = "block"
+    document.getElementById (crapsBettingGridContainer).style.display = "block"
+    canChangeBet = true
+    setBetAmount(minimumBet)
+    
 }
 
 function setMoney (money) {
@@ -170,7 +176,7 @@ function processedDiceResult (diceResult) {
 }
 
 function exitGame () {
-    alert("After playing" + currentRounds + " rounds, you leave with" + currentMoney + "$!")
+    alert("After playing " + currentRounds + " rounds, you leave with" + currentMoney + "$")
     hideMainGameSection()
     showRegistrationPane()
     document.getElementById(crapsUsernameInput).value = ""
